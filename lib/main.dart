32a1401b7803e0db541eb02790b85e5e142f83e8
wogks <wogks27@naver.com>
2022-07-12 01:06:29 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const BaeminMain(),
+    return const MaterialApp(
+      home: BaeminMain(),
     );
   }
 }
@@ -25,53 +25,100 @@ class BaeminMain extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: const Color(0xFF5EBEBB),
-        title: const Text('경기 안양시 동안구 동...'),
+        title: 
+        const Text('경기 안양시 동안구 동...',),
         leading: const Icon(Icons.account_tree_outlined),
         actions: [
           IconButton(onPressed: (){}, icon: const Icon(Icons.notifications)),
           IconButton(onPressed: (){}, icon: const Icon(Icons.notifications))
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Color(0xFF5EBEBB),
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15))
-            ),
-            height: 50,
-            child: const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  border: UnderlineInputBorder(),
-                  hintText: '닭발, 곱창 등',hintStyle: TextStyle(color:Color(0xFF424242))
+      body: ListView(
+        children: [Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFF5EBEBB),
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15))
+              ),
+              height: 50,
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: UnderlineInputBorder(),
+                    hintText: '닭발, 곱창 등',hintStyle: TextStyle(color:Color(0xFF424242))
+                  ),
                 ),
               ),
             ),
-          ),
-          Row(
-            children: [
-              Flexible(
-                child: Card(child: Container(
-                  height: 200,
-                  color: Colors.redAccent,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: Card(child: Container(
+                      height: 200,
+                      color: Colors.redAccent,
+                    ),
+                    ),
+                  ),        
+                  Flexible(
+                    child: Card(child: Container(
+                      height: 200,
+                  
+                      color: Colors.redAccent,
+                    ),
+                    ),
+                  ),         
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blue
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 130,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.green
                 ),
-              ),        
-              Flexible(
-                child: Card(child: Container(
-                  height: 200,
-              
-                  color: Colors.redAccent,
+                child:  Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('data'),
+                            Text('data'),
+                            Text('data')
+                          ],
+                        ),
+                      ),
+                    ),
+                    Image.asset('assets/sd.png',
+                    width: 100,),
+                    SizedBox(
+                      width: 8,
+                    )
+                  ],
                 ),
-                ),
-              ),         
-            ],
-          ),
-          
-        ],
+              ),
+            ),
+          ],
+        ),
+        ]
       ),
     );
   }
