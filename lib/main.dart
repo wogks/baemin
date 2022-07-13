91d891202a8377f8c9188256f1b7f2f62993e06a
wogks <wogks27@naver.com>
2022-07-13 14:21:46 +0900

    가운데 스택

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class BaeminMain extends StatelessWidget {
         leading: const Icon(Icons.account_tree_outlined),
         actions: [
           IconButton(onPressed: (){}, icon: const Icon(Icons.notifications)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.notifications))
+          IconButton(onPressed: (){}, icon: const Icon(Icons.percent))
         ],
       ),
       body: ListView(
@@ -90,6 +90,7 @@ class BaeminMain extends StatelessWidget {
                     ],
                   ),
                 ),
+                
                 Positioned(
                   left:30, top: 50,
                   child: Text('배달',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)),
@@ -102,6 +103,54 @@ class BaeminMain extends StatelessWidget {
                 Positioned(
                   right: 40, top: 100,
                   child: Text('한번에 한집만 배달해요',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15)))
+              ],
+            ),
+             Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.red
+                    ),
+                    child:  Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('d'),
+                                Text('d'),
+                                
+                              ],
+                            ),
+                          ),
+                        ),
+                        Image.asset('assets/sd.png',
+                        width: 100,),
+                        const SizedBox(
+                          width: 8,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 20, right: 20,
+                  child: Container(
+                              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black.withOpacity(0.6)
+                              ),
+                              child: const Padding(
+                  padding: EdgeInsets.fromLTRB(16, 4, 16, 4),
+                  child: Text('4 / 6 모두보기',style: TextStyle( color: Colors.white),),
+                              ),
+                            ),
+                ),
               ],
             ),
             Padding(
@@ -206,7 +255,7 @@ class BaeminMain extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Container(
                 
-                height: 90,
+                height: 80,
                 child: Card(   
                   shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15.0)
